ccdbe4932d799ff10697e42d134e050ed40709c6
Jayme Gomes <jayme.r.g.n@hotmail.com>
2021-08-06 10:18:51 -0300

    first commit

---
+++ lib/src/main/java/exemplos/uri1002/Circulo.java
@@ -0,0 +1,46 @@
+/*
+ * Direitos Autorais, 2021, Jayme Gomes.
+ */
+package exemplos.uri1002;
+
+/**
+ * A classe <code>Circulo</code> representa um círculo com raio.
+ * 
+ * @author Jayme
+ *
+ */
+public class Circulo {
+	
+	/*
+	 * 
+	 */
+	private double raio;
+
+	/*
+	 * 
+	 */
+	private static final double PI = 3.14159;
+
+	/**
+	 * Cria um círculo com raio <code>raio</code>.
+	 * 
+	 * @param raio o raio do novo círculo
+	 * @throws IllegalArgumentException
+	 * 			Quando o valor do raio é menor ou igual a zero.
+	 */
+	public Circulo(double raio) {
+		super();
+		if (raio <= 0.0) {
+			throw new IllegalArgumentException();
+		}
+		this.raio = raio;
+	}
+
+	/**
+	 * @return the area
+	 */
+	public double getArea() {
+		return raio * raio * PI;
+	}
+
+}
