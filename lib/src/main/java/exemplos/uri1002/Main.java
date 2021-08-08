8aa1e796086a92d73055ce867f6146188806a730
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-08-08 14:08:14 -0300

    Add Classe Square e SquareTest

--- lib/src/main/java/exemplos/uri1002/Main.java
+++ lib/src/main/java/exemplos/uri1002/Main.java
@@ -16,18 +16,26 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Scanner teclado;
-		double raio;
+		double raio, lado;
 		Circle c1;
+		Square s1;
 		double area;
 		
 		teclado = new Scanner(System.in);
 		raio = teclado.nextDouble();
+		lado = teclado.nextDouble();
 		teclado.close();
 		
 		c1 = new Circle(raio);
 		area = c1.getArea();
 		
-		System.out.printf("A = %.4f\n", area);
+		System.out.printf("A = %.3f\n", area);
+
+		s1 = new Square(lado);
+		area = s1.getArea();
+
+		System.out.printf("A = %.3f\n", area);
+
 	}
 
 }
---
+++ lib/src/main/java/exemplos/uri1002/Square.java
@@ -0,0 +1,38 @@
+package exemplos.uri1002;
+/**
+ * 
+ * @author Chrystian Rocha
+ *
+ */
+public class Square extends GeometricShapes{
+	double lado;
+	/**
+	 * Cria um quadrado com lado <code>lado</code>.
+	 * @param lado
+	 * @throws IllegalArgumentException
+	 * 			Quando o valor do lado é menor ou igual a zero.
+	 */
+	public Square(double lado) {
+		super();
+		if(lado <= 0.0) {
+			throw new IllegalArgumentException();
+		}
+		this.lado = lado;
+	}
+	
+	/**
+	 * return area
+	 */
+	@Override
+	Double getArea() {
+		// TODO Auto-generated method stub
+		return lado * lado;
+	}
+
+	@Override
+	String showAttributes() {
+		// TODO Auto-generated method stub
+		return "Attributes: lado";
+	}
+
+}
